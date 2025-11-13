017c6b4aa4794891f98bb7233b47ceae8c3a2066
gijsdevries <gijsdvries21@gmail.com>
2025-11-13 10:23:01 +0100

    added blinky

---
+++ build/esp-idf/mbedtls/mbedtls/library/version_features.c
@@ -0,0 +1,1 @@
+/home/gijs/esp/esp-idf-v5.5.1/components/mbedtls/mbedtls/library/version_features.c
--- main/main.cpp
+++ main/main.cpp
@@ -1,12 +1,28 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "driver/gpio.h"
 #include "sdkconfig.h"
 
-extern "C" {void app_main(void){
-	while (1) 
-	{
-        	printf("Hello World\n");
-        	vTaskDelay(1000 / portTICK_PERIOD_MS); // Delay 1 second
-    	}
+//#define LED_BLINK (gpio_num_t)3;
+gpio_num_t LED_BLINK = (gpio_num_t)2;
+
+extern "C" {void app_main(void)
+{
+    // Configure the GPIO pin 
+    gpio_reset_pin(LED_BLINK); 
+    gpio_set_direction(LED_BLINK, GPIO_MODE_OUTPUT);
+
+    // Blink loop
+    while (1) {
+        // Turn LED ON
+        printf("LED ON\n");
+        gpio_set_level(LED_BLINK, 1);
+        vTaskDelay(1000 / portTICK_PERIOD_MS); // Delay 1 second
+
+        // Turn LED OFF
+        printf("LED OFF\n");
+        gpio_set_level(LED_BLINK, 0);
+        vTaskDelay(1000 / portTICK_PERIOD_MS); // Delay 1 second
+    }
 }}
