800d028eff65b9cd50b7c57a0172694e1df5cc6f
gijsdevries <gijsdvries21@gmail.com>
2025-11-22 14:33:41 +0100

    cleanup

--- main/main.cpp
+++ main/main.cpp
@@ -18,45 +18,44 @@ gpio_num_t LED_BLINK = (gpio_num_t)2;
 i2c_master_bus_handle_t bus_handle;
 i2c_master_dev_handle_t dev_handle;
 
+uint8_t i2c_distance();
 uint8_t i2c_read(uint8_t msg_type);
 static void i2c_write(uint8_t msg_type);
 static void i2c_master_init(i2c_master_bus_handle_t *bus_handle, i2c_master_dev_handle_t *dev_handle);
 
 enum i2c_msg_types {
+  /* writing */
   SYSRANGE__START,
   SYSTEM__INTERRUPT_CLEAR,
+
+  /* reading */
   RESULT__INTERRUPT_STATUS_GPIO,
+  RESULT__RANGE_VAL,
 };
 
 extern "C" {void app_main(void) {
   i2c_master_init(&bus_handle, &dev_handle);
-  uint8_t datasend1[3] = {0x00, 0x18, 0x01};
 
-  while(1) { 
-    i2c_write(SYSRANGE__START);
-
-    uint8_t datasend[2] = {0x00, 0x62};
-    size_t lensend = sizeof(datasend); 
-
-    uint8_t datarec = 0; 
-    size_t lenrec = sizeof(datarec);
-
-    uint8_t datasend3[2] = {0x00, 0x4F};
-    size_t lensend3 = sizeof(datasend3); 
-
-    uint8_t ITR_STATUS = i2c_read(RESULT__INTERRUPT_STATUS_GPIO);
-
-    while (ITR_STATUS != 4) {
-      ITR_STATUS = i2c_read(RESULT__INTERRUPT_STATUS_GPIO);
-    }
-
-    datasend[1] = 0x62;
-    ESP_ERROR_CHECK(i2c_master_transmit_receive(dev_handle, datasend, lensend, &datarec, lenrec, I2C_MASTER_TIMEOUT_MS / portTICK_PERIOD_MS));
-    printf("distance: %d\n", datarec);
-    i2c_write(SYSTEM__INTERRUPT_CLEAR);
+  while(1) {
+    uint8_t distance = i2c_distance();
+    printf("distance: %d\n", distance);
     vTaskDelay(100 / portTICK_PERIOD_MS); 
   }
 }}
+
+uint8_t i2c_distance() {
+  i2c_write(SYSRANGE__START);
+
+  uint8_t ITR_STATUS = i2c_read(RESULT__INTERRUPT_STATUS_GPIO);
+
+  while (ITR_STATUS != 4) {
+    ITR_STATUS = i2c_read(RESULT__INTERRUPT_STATUS_GPIO);
+  }
+
+  uint8_t distance = i2c_read(RESULT__RANGE_VAL);
+  i2c_write(SYSTEM__INTERRUPT_CLEAR);
+  return distance;
+}
 
 static void i2c_master_init(i2c_master_bus_handle_t *bus_handle, i2c_master_dev_handle_t *dev_handle)
 {
@@ -83,6 +82,12 @@ uint8_t i2c_read(uint8_t msg_type) {
     case RESULT__INTERRUPT_STATUS_GPIO: 
       {
         uint8_t msg[2] = {0x00, 0x4F};
+        ESP_ERROR_CHECK(i2c_master_transmit_receive(dev_handle, msg, 2, &i2c_recieved, 1, I2C_MASTER_TIMEOUT_MS / portTICK_PERIOD_MS));
+        break;
+      }
+    case RESULT__RANGE_VAL: 
+      {
+        uint8_t msg[2] = {0x00, 0x62};
         ESP_ERROR_CHECK(i2c_master_transmit_receive(dev_handle, msg, 2, &i2c_recieved, 1, I2C_MASTER_TIMEOUT_MS / portTICK_PERIOD_MS));
         break;
       }
