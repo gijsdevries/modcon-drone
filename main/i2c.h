3a71f0abe2bb4056b335cc02fd8170aa78b971c8
gijsdevries <gijsdvries21@gmail.com>
2025-11-22 16:08:43 +0100

    this works

--- main/i2c.h
+++ main/i2c.h
@@ -2,6 +2,8 @@
 #define I2C_H
 
 #include "driver/i2c_master.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
 #define I2C_MASTER_SCL_IO           (gpio_num_t)22              /*!< GPIO number used for I2C master clock */
 #define I2C_MASTER_SDA_IO           (gpio_num_t)21              /*!< GPIO number used for I2C master data  */
@@ -16,7 +18,6 @@ extern i2c_master_bus_handle_t bus_handle;
 uint8_t i2c_distance();
 uint8_t i2c_read(uint8_t msg_type);
 static void i2c_write(uint8_t msg_type);
-//static void i2c_master_init(i2c_master_bus_handle_t *bus_handle, i2c_master_dev_handle_t *dev_handle);
 
 enum i2c_msg_types {
   /* writing */
