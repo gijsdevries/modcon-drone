2befe8009486909ce4ee751e523f93cff139395d
gijsdevries <gijsdvries21@gmail.com>
2025-11-22 18:05:20 +0100

    example working

--- main/main.cpp
+++ main/main.cpp
@@ -1,21 +1,51 @@
 #include <stdio.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
+#include "driver/ledc.h"
+#include "esp_err.h"
 #include "driver/gpio.h"
-#include "sdkconfig.h"
-#include "esp_log.h"
 
-/* USER INCLUDES */
-#include "i2c.h"
+#define LEDC_TIMER              LEDC_TIMER_0
+#define LEDC_MODE               LEDC_LOW_SPEED_MODE
+#define LEDC_OUTPUT_IO          (gpio_num_t)5 // Define the output GPIO
+#define LEDC_CHANNEL            LEDC_CHANNEL_0
+#define LEDC_DUTY_RES           LEDC_TIMER_13_BIT // Set duty resolution to 13 bits
+#define LEDC_DUTY               (4096) // Set duty to 50%. (2 ** 13) * 50% = 4096
+#define LEDC_FREQUENCY          (4000) // Frequency in Hertz. Set frequency at 4 kHz
 
-extern "C" {void app_main(void) {
-  i2c_master_init();
+void example_ledc_init(void)
+{
+    // Prepare and then apply the LEDC PWM timer configuration
+    ledc_timer_config_t ledc_timer = {
+        .speed_mode       = LEDC_MODE,
+        .duty_resolution  = LEDC_DUTY_RES,
+        .timer_num        = LEDC_TIMER,
+        .freq_hz          = LEDC_FREQUENCY,  // Set output frequency at 4 kHz
+        .clk_cfg          = LEDC_AUTO_CLK,
+        .deconfigure      = 0
+    };
+    ESP_ERROR_CHECK(ledc_timer_config(&ledc_timer));
 
-  while(1) {
-    uint8_t distance = i2c_distance(); 
-    printf("distance: %d\n", distance);
-    vTaskDelay(100 / portTICK_PERIOD_MS); 
-  }
+    // Prepare and then apply the LEDC PWM channel configuration
+    ledc_channel_config_t ledc_channel = {
+        .gpio_num       = LEDC_OUTPUT_IO,
+        .speed_mode     = LEDC_MODE,
+        .channel        = LEDC_CHANNEL,
+        .intr_type      = LEDC_INTR_DISABLE,
+        .timer_sel      = LEDC_TIMER,
+        .duty           = 0, // Set duty to 0%
+        .hpoint         = 0,
+        .flags          = {
+            .output_invert = 0
+        }
+    };
+    ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel));
+}
+
+extern "C" { void app_main(void)
+{
+    // Set the LEDC peripheral configuration
+    example_ledc_init();
+    // Set duty to 50%
+    ESP_ERROR_CHECK(ledc_set_duty(LEDC_MODE, LEDC_CHANNEL, LEDC_DUTY));
+    // Update duty to apply the new value
+    ESP_ERROR_CHECK(ledc_update_duty(LEDC_MODE, LEDC_CHANNEL));
 }}
-
-
