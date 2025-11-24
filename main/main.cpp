8aba36e532fca7e8a3ae02a55a011d2e7038e148
gijsdevries <gijsdvries21@gmail.com>
2025-11-24 23:32:14 +0100

    init should be good

--- main/main.cpp
+++ main/main.cpp
@@ -1,28 +1,44 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "driver/gpio.h"
 #include "sdkconfig.h"
+#include "esp_now.h"
+#include "esp_wifi.h"
+#include "nvs_flash.h"
 
 // Define the GPIO pin for the LED (GPIO 2 is common for onboard LEDs)
 #define BLINK_GPIO (gpio_num_t)2
+#define ESPNOW_WIFI_MODE WIFI_MODE_STA
+#define ESPNOW_WIFI_IF   ESP_IF_WIFI_STA
 
-extern "C" {void app_main(void)
+static void example_wifi_init(void)
 {
-    // Configure the GPIO pin
-    gpio_reset_pin(BLINK_GPIO);
-    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
+    ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
+    ESP_ERROR_CHECK( esp_wifi_set_mode(ESPNOW_WIFI_MODE) );
+    ESP_ERROR_CHECK( esp_wifi_start());
+}
+extern "C" {void app_main(void) {
 
-    // Blink loop
-    while (1) {
-        // Turn LED ON
-        printf("LED ON\n");
-        gpio_set_level(BLINK_GPIO, 1);
-        vTaskDelay(1000 / portTICK_PERIOD_MS); // Delay 1 second
+  // Initialize NVS
+  esp_err_t ret = nvs_flash_init();
+  if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+    ESP_ERROR_CHECK( nvs_flash_erase() );
+    ret = nvs_flash_init();
+  }
+  ESP_ERROR_CHECK( ret );
 
-        // Turn LED OFF
-        printf("LED OFF\n");
-        gpio_set_level(BLINK_GPIO, 0);
-        vTaskDelay(1000 / portTICK_PERIOD_MS); // Delay 1 second
-    }
+  example_wifi_init();
+  esp_now_init(); 
+
+  uint8_t mac_adr[6] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+  uint8_t distance = 12;
+
+  while (1) {
+//    ESP_ERROR_CHECK(esp_now_send(mac_adr, &distance, 1));
+    vTaskDelay(1000 / portTICK_PERIOD_MS); // Delay 1 second
+  }
 }}
