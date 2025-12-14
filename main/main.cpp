9dd406fec4b97534e28d6990203cd32254f91162
gijsdevries <gijsdvries21@gmail.com>
2025-12-14 20:36:58 +0100

    recieving val 100

--- main/main.cpp
+++ main/main.cpp
@@ -1,21 +1,64 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include <esp_now.h>
 #include "driver/gpio.h"
 #include "sdkconfig.h"
 #include "esp_log.h"
+#include "esp_wifi.h"
+#include "nvs_flash.h"
 
 /* USER INCLUDES */
 #include "i2c.h"
 
+#define ESPNOW_WIFI_MODE WIFI_MODE_STA
+#define ESPNOW_WIFI_IF   WIFI_IF_STA
+
+typedef struct struct_message {
+  int distance;
+} struct_message;
+
+struct_message myData;
+
+static void example_wifi_init(void)
+{
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
+    ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
+    ESP_ERROR_CHECK( esp_wifi_set_mode(ESPNOW_WIFI_MODE) );
+    ESP_ERROR_CHECK( esp_wifi_start());
+//    ESP_ERROR_CHECK( esp_wifi_set_channel(CONFIG_ESPNOW_CHANNEL, WIFI_SECOND_CHAN_NONE));
+}
+
+// callback function that will be executed when data is received
+void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
+  memcpy(&myData, incomingData, sizeof(myData));
+  printf("recieved distance = %d\n", myData.distance);
+}
+
 extern "C" {void app_main(void) {
   i2c_master_init();
 
+  // Initialize NVS
+  esp_err_t ret = nvs_flash_init();
+  if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+    ESP_ERROR_CHECK( nvs_flash_erase() );
+    ret = nvs_flash_init();
+  }
+  ESP_ERROR_CHECK( ret );
+
+  example_wifi_init();
+  ESP_ERROR_CHECK( esp_now_init() );
+
+  esp_now_register_recv_cb(esp_now_recv_cb_t(OnDataRecv));
+
+
   while(1) {
-    uint8_t distance = i2c_distance(); //push in mai?
-    printf("distance: %d\n", distance);
+    //    uint8_t distance = i2c_distance(); //push in mai?
+    //    printf("distance: %d\n", distance);
+    printf("hello world\n");
     vTaskDelay(1000 / portTICK_PERIOD_MS); 
   }
 }}
-
-
