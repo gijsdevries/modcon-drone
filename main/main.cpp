16f6d8e645334d2871386f73774d604f50227e09
gijsdevries <gijsdvries21@gmail.com>
2025-12-14 20:06:44 +0100

    deliviry succes

--- main/main.cpp
+++ main/main.cpp
@@ -1,13 +1,6 @@
-/* UART asynchronous example, that uses separate RX and TX tasks
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "esp_wifi.h"
 #include "esp_system.h"
 #include "esp_log.h"
 #include "driver/uart.h"
@@ -15,57 +8,42 @@
 #include "driver/gpio.h"
 #include "stdio.h"
 #include "string.h"
+#include <esp_now.h>
+#include "nvs_flash.h"
 
-static const int RX_BUF_SIZE = 1024;
+#define ESPNOW_WIFI_MODE WIFI_MODE_STA
+#define ESPNOW_WIFI_IF   WIFI_IF_STA
 
-#define RXD_PIN (gpio_num_t)3
+// REPLACE WITH YOUR RECEIVER MAC Address
+uint8_t broadcastAddress[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
 
-void init(void)
-{
-    const uart_config_t uart_config = {
-        .baud_rate = 115200,
-        .data_bits = UART_DATA_8_BITS,
-        .parity = UART_PARITY_DISABLE,
-        .stop_bits = UART_STOP_BITS_1,
-        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
-        .source_clk = UART_SCLK_DEFAULT,
-    };
-    // We won't use a buffer for sending data.
-    uart_driver_install(UART_NUM_1, RX_BUF_SIZE * 2, 0, 0, NULL, 0);
-    uart_param_config(UART_NUM_1, &uart_config);
-    uart_set_pin(UART_NUM_1, -1, RXD_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+// Structure example to send data
+// Must match the receiver structure
+typedef struct struct_message {
+  int distance;
+} struct_message;
+
+// Create a struct_message called myData
+struct_message myData;
+
+esp_now_peer_info_t peerInfo;
+
+// callback when data is sent
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  printf("\r\nLast Packet Send Status:\t");
+  printf(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success\n" : "Delivery Fail\n");
 }
 
-static void rx_task(void *arg)
+static void example_wifi_init(void)
 {
-    static const char *RX_TASK_TAG = "RX_TASK";
-    esp_log_level_set(RX_TASK_TAG, ESP_LOG_INFO);
-    char* data = (char*) malloc(RX_BUF_SIZE + 1);
-    while (1) {
-      const int rxBytes = uart_read_bytes(UART_NUM_1, data, RX_BUF_SIZE, 1000 / portTICK_PERIOD_MS);
-      if (rxBytes > 0) {
-        int distance = atoi(data);
-
-        if (distance >= 10 && distance <= 200) {
-          //valid arg
-          printf("valid height: %d\n", distance);
-          gpio_set_level((gpio_num_t)2, 1);
-          vTaskDelay(1000 / portTICK_PERIOD_MS); // Delay 1 second
-          gpio_set_level((gpio_num_t)2, 0);
-        }
-        else {
-          printf("invalid height: %d\n", distance);
-          for (char i=0; i < 10; i++) {
-            gpio_set_level((gpio_num_t)2, 1);
-            vTaskDelay(50 / portTICK_PERIOD_MS); // Delay 1 second
-            gpio_set_level((gpio_num_t)2, 0);
-            vTaskDelay(50 / portTICK_PERIOD_MS); // Delay 1 second
-          }
-        }
-        data[rxBytes] = 0;
-      }
-    }
-    free(data);
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
+    ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
+    ESP_ERROR_CHECK( esp_wifi_set_mode(ESPNOW_WIFI_MODE) );
+    ESP_ERROR_CHECK( esp_wifi_start());
+//    ESP_ERROR_CHECK( esp_wifi_set_channel(CONFIG_ESPNOW_CHANNEL, WIFI_SECOND_CHAN_NONE));
 }
 
 extern "C" {void app_main(void)
@@ -73,6 +51,45 @@ extern "C" {void app_main(void)
     gpio_reset_pin((gpio_num_t)2);
     gpio_set_direction((gpio_num_t)2, GPIO_MODE_OUTPUT);
 
-    init();
-    xTaskCreate(rx_task, "uart_rx_task", 4096, NULL, configMAX_PRIORITIES - 1, NULL);
+
+    // Initialize NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK( nvs_flash_erase() );
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK( ret );
+
+    example_wifi_init();
+    ESP_ERROR_CHECK( esp_now_init() );
+
+    // Once ESPNow is successfully Init, we will register for Send CB to
+    // get the status of Trasnmitted packet
+    esp_now_register_send_cb(esp_now_send_cb_t(OnDataSent));
+
+    // Register peer
+    memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+    peerInfo.channel = 0;  
+    peerInfo.encrypt = false;
+
+    // Add peer        
+    if (esp_now_add_peer(&peerInfo) != ESP_OK){
+      printf("Failed to add peer\n");
+      return;
+    }
+
+    while (1) {
+      myData.distance = 100;
+
+      // Send message via ESP-NOW
+      esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
+
+      if (result == ESP_OK) {
+        printf("Sent with success");
+      }
+      else {
+        printf("Error sending the data");
+      }
+      vTaskDelay(2000 / portTICK_PERIOD_MS);
+    }
   }}
