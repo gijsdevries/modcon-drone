303d6e2eb461ce5a76d28e2adabe85daf4c61646
gijsdevries <gijsdvries21@gmail.com>
2025-12-23 20:00:34 +0100

    pid should be done now not tested yet

--- main/main.cpp
+++ main/main.cpp
@@ -11,10 +11,16 @@
 #include "espnow_rec.h"
 #include "pwm.h"
 
-#define BUILTIN_LED (gpio_num_t)2
-//#define DEBUG
+//TODO calibrate these values
+#define KP 0.1
+#define KI 0.1
+#define KD 0.1
+#define dT 0.01
 
-float desired_distance, actual_distance, pwm;
+#define BUILTIN_LED (gpio_num_t)2
+#define DEBUG
+
+float error, error_sum, error_div, error_prev, desired_distance, actual_distance, pwm;
 
 extern "C" {void app_main(void) {
   esp_now_full_init();
@@ -34,23 +40,24 @@ extern "C" {void app_main(void) {
   while (1) {
     actual_distance = hc_sr04_measure_cm(sensor); 
 
-    if (desired_distance < actual_distance && pwm > 1) {
-      pwm--;
-    }
-    else if (desired_distance > actual_distance && pwm < 255) {
-      pwm++;
-    }
+    error = desired_distance - actual_distance;
+    actual_distance = hc_sr04_measure_cm(sensor); 
+    error_sum += error * dT;
+    error_div = (error - error_prev) / dT;
+    pwm = error*KP + error_sum*KI + error_div*KD;
     setPWM(pwm);
+    error_prev = error;
+
+    vTaskDelay(10 / portTICK_PERIOD_MS);
 
 #ifdef DEBUG
     static int i = 0;
     i++;
     if (i > 250) {
-      printf("desired distance: %d    actual distance: %d   pwm: %d\n", desired_distance, actual_distance, pwm);
+      printf("error: %f   error_sum: %f   error_div: %f   error_prev: %f    desired_distance: %f    actual_distance: %f   pwm: %f", 
+      error, error_sum, error_div, error_prev, desired_distance, actual_distance, pwm);
       i = 0;
     } 
 #endif
-
-    vTaskDelay(10 / portTICK_PERIOD_MS);
   }
 }}
