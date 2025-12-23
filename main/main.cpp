a24c605daa2148410ea70067af0d94461875e0a8
gijsdevries <gijsdvries21@gmail.com>
2025-12-23 11:24:50 +0100

    code builds

--- main/main.cpp
+++ main/main.cpp
@@ -11,6 +11,7 @@
 /* USER INCLUDES */
 #include "i2c.h"
 #include "hc_sr04.h"
+#include "pwm.h"
 
 
 extern "C" {void app_main(void) {
@@ -21,20 +22,18 @@ extern "C" {void app_main(void) {
   };
   hc_sr04_handle_t sensor = hc_sr04_init(&config);
 
+//  example_ledc_init();
 
   while(1) {
     float distance = hc_sr04_measure_cm(sensor);
+//    setPWM(distance);
     printf("distance: %f\n", distance);
     vTaskDelay(500 / portTICK_PERIOD_MS);
   }
 }}
 
+
 /*
-
-/* USER INCLUDES */
-#include "i2c.h"
-#include "pwm.h"
-
 extern "C" {void app_main(void) {
   i2c_master_init();
   i2c_write(SYSTEM__INTERRUPT_CONFIG_GPIO); 
@@ -45,4 +44,4 @@ extern "C" {void app_main(void) {
     setPWM(distance);
     printf("distance: %d\n", distance);
     vTaskDelay(100 / portTICK_PERIOD_MS); 
-*/
+  */
