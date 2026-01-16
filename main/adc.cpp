6e7bb4784f1851755843a89746c10ee856a48c76
gijsdevries <gijsdvries21@gmail.com>
2026-01-16 10:13:45 +0100

    reading out adc

---
+++ main/adc.cpp
@@ -0,0 +1,24 @@
+#include "adc.h"
+
+adc_oneshot_unit_handle_t adc1_handle;
+static int padc_raw[2][10];
+
+void adc_init() {
+  //Vref 3.3V   12 bit
+  adc_oneshot_unit_init_cfg_t init_config1 = {
+    .unit_id = ADC_UNIT_1,
+  };
+  ESP_ERROR_CHECK(adc_oneshot_new_unit(&init_config1, &adc1_handle));
+
+  adc_oneshot_chan_cfg_t config = {
+    .atten = EXAMPLE_ADC_ATTEN,
+    .bitwidth = ADC_BITWIDTH_DEFAULT,
+  };
+  ESP_ERROR_CHECK(adc_oneshot_config_channel(adc1_handle, EXAMPLE_ADC1_CHAN0, &config));
+}
+
+int read_adc() {
+  ESP_ERROR_CHECK(adc_oneshot_read(adc1_handle, EXAMPLE_ADC1_CHAN0, &padc_raw[0][0]));
+
+  return padc_raw[0][0];
+}
--- main/main.cpp
+++ main/main.cpp
@@ -5,36 +5,19 @@
 #include "sdkconfig.h"
 #include "esp_log.h"
 #include "hc_sr04.h"
-#include "esp_adc/adc_oneshot.h"
-#include "esp_adc/adc_cali.h"
-#include "esp_adc/adc_cali_scheme.h"
-
 /* USER INCLUDES */
 #include "i2c.h"
 #include "espnow_rec.h"
 #include "pwm.h"
+#include "adc.h"
 #include "uart.h"
-
-#define EXAMPLE_ADC_ATTEN           ADC_ATTEN_DB_12
-#define EXAMPLE_ADC1_CHAN0          ADC_CHANNEL_4
 
 extern "C" {void app_main(void) {
   //TODO add adc read tester
   //TODO add mpu read tester
 
-  adc_oneshot_unit_handle_t adc1_handle;
-  adc_oneshot_unit_init_cfg_t init_config1 = {
-    .unit_id = ADC_UNIT_1,
-  };
-  ESP_ERROR_CHECK(adc_oneshot_new_unit(&init_config1, &adc1_handle));
-
-  adc_oneshot_chan_cfg_t config = {
-    .atten = EXAMPLE_ADC_ATTEN,
-    .bitwidth = ADC_BITWIDTH_DEFAULT,
-  };
-  ESP_ERROR_CHECK(adc_oneshot_config_channel(adc1_handle, EXAMPLE_ADC1_CHAN0, &config));
-
-
+  adc_init();
+  
   hc_sr04_config_t config_hc = {
     .trigger_pin = GPIO_NUM_4,
     .echo_pin = GPIO_NUM_5,
@@ -47,12 +30,13 @@ extern "C" {void app_main(void) {
 
   uint8_t i = 0;
   float actual_distance = 0;
-  static int adc_raw[2][10];
+
+  int adc;
 
   while (1) {
 
-    ESP_ERROR_CHECK(adc_oneshot_read(adc1_handle, EXAMPLE_ADC1_CHAN0, &adc_raw[0][0]));
-    printf("ADC%d Channel[%d] Raw Data: %d\n", ADC_UNIT_1 + 1, EXAMPLE_ADC1_CHAN0, adc_raw[0][0]);
+    adc = read_adc();
+    printf("ADC%d Channel[%d] Raw Data: %d\n", ADC_UNIT_1 + 1, EXAMPLE_ADC1_CHAN0, adc);
 
 
     i++;
