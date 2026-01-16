833ba4e3eb4e4a7518c1cad5c51ec046ef9e65ff
gijsdevries <gijsdvries21@gmail.com>
2026-01-16 09:47:28 +0100

    reading out adc

--- main/main.cpp
+++ main/main.cpp
@@ -5,7 +5,9 @@
 #include "sdkconfig.h"
 #include "esp_log.h"
 #include "hc_sr04.h"
-#include "hal/adc_types.h"
+#include "esp_adc/adc_oneshot.h"
+#include "esp_adc/adc_cali.h"
+#include "esp_adc/adc_cali_scheme.h"
 
 /* USER INCLUDES */
 #include "i2c.h"
@@ -13,29 +15,52 @@
 #include "pwm.h"
 #include "uart.h"
 
+#define EXAMPLE_ADC_ATTEN           ADC_ATTEN_DB_12
+#define EXAMPLE_ADC1_CHAN0          ADC_CHANNEL_4
+
 extern "C" {void app_main(void) {
   //TODO add adc read tester
   //TODO add mpu read tester
-  hc_sr04_config_t config = {
+
+  adc_oneshot_unit_handle_t adc1_handle;
+  adc_oneshot_unit_init_cfg_t init_config1 = {
+    .unit_id = ADC_UNIT_1,
+  };
+  ESP_ERROR_CHECK(adc_oneshot_new_unit(&init_config1, &adc1_handle));
+
+  adc_oneshot_chan_cfg_t config = {
+    .atten = EXAMPLE_ADC_ATTEN,
+    .bitwidth = ADC_BITWIDTH_DEFAULT,
+  };
+  ESP_ERROR_CHECK(adc_oneshot_config_channel(adc1_handle, EXAMPLE_ADC1_CHAN0, &config));
+
+
+  hc_sr04_config_t config_hc = {
     .trigger_pin = GPIO_NUM_4,
     .echo_pin = GPIO_NUM_5,
     .timeout_us = 30000
   };
 
-  hc_sr04_handle_t sensor = hc_sr04_init(&config);
+  hc_sr04_handle_t sensor = hc_sr04_init(&config_hc);
 
   pwm_init();
 
   uint8_t i = 0;
   float actual_distance = 0;
+  static int adc_raw[2][10];
 
   while (1) {
+
+    ESP_ERROR_CHECK(adc_oneshot_read(adc1_handle, EXAMPLE_ADC1_CHAN0, &adc_raw[0][0]));
+    printf("ADC%d Channel[%d] Raw Data: %d\n", ADC_UNIT_1 + 1, EXAMPLE_ADC1_CHAN0, adc_raw[0][0]);
+
+
     i++;
-    
-    actual_distance = hc_sr04_measure_cm(sensor);
+
+    //actual_distance = hc_sr04_measure_cm(sensor);
     setPWM(i);
 
-    printf("actual_distance: %f\n", actual_distance);
-    vTaskDelay(portTICK_PERIOD_MS / 100);
+    //printf("actual_distance: %f\n", actual_distance);
+    vTaskDelay(1000 / portTICK_PERIOD_MS); 
   }
 }}
