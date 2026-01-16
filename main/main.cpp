cee944067830a40382b6ce3f911804f4f3548154
gijsdevries <gijsdvries21@gmail.com>
2026-01-16 09:33:12 +0100

    added adc component

--- main/main.cpp
+++ main/main.cpp
@@ -5,6 +5,7 @@
 #include "sdkconfig.h"
 #include "esp_log.h"
 #include "hc_sr04.h"
+#include "hal/adc_types.h"
 
 /* USER INCLUDES */
 #include "i2c.h"
@@ -12,121 +13,29 @@
 #include "pwm.h"
 #include "uart.h"
 
-#define dT 0.01
-
-#define DEBUG_PRINT_INTERVAL 250
-#define MIN_RANGE 0.05
-#define MAX_RANGE 2
-
-#define MAX_PWM 100
-#define MIN_PWM 0
-
-#define BUILTIN_LED (gpio_num_t)2
-#define DEBUG
-
-float error, error_sum, error_div, error_prev, desired_distance, actual_distance, pwm, output;
-bool operation_state;
-float kp, ki, kd;
-
 extern "C" {void app_main(void) {
-  desired_distance = 1.8;
-  operation_state = false;
-
-  kp = 0;
-  ki = 0;
-  kd = 0;
-
-  pid_struct pid_struct;
-  pid_struct.msg_type = PID_DRONE;
-
-  esp_now_full_init();
-
+  //TODO add adc read tester
+  //TODO add mpu read tester
   hc_sr04_config_t config = {
     .trigger_pin = GPIO_NUM_4,
     .echo_pin = GPIO_NUM_5,
     .timeout_us = 30000
   };
+
   hc_sr04_handle_t sensor = hc_sr04_init(&config);
 
   pwm_init();
 
-  uart_init();
-
-  gpio_reset_pin(BUILTIN_LED);
-  gpio_set_direction(BUILTIN_LED, GPIO_MODE_OUTPUT);
-
-  sendData("0");
-  vTaskDelay(3000 / portTICK_PERIOD_MS);
-
-  char str[16];
-
-  bool led_state = true;
-
-  while (kp == 0 && ki == 0 && kd == 0) {
-    gpio_set_level((gpio_num_t)2, led_state);
-    led_state = !led_state; 
-    vTaskDelay(200 / portTICK_PERIOD_MS);
-  }
-  printf("recieved PID values:\nkp = %f\nki = %f\nkd = %f\n", kp, ki, kd);
+  uint8_t i = 0;
+  float actual_distance = 0;
 
   while (1) {
-    gpio_set_level((gpio_num_t)2, operation_state);
+    i++;
+    
+    actual_distance = hc_sr04_measure_cm(sensor);
+    setPWM(i);
 
-    if (operation_state == false) {
-      sendData(0);
-      vTaskDelay(10 / portTICK_PERIOD_MS);
-    }
-    else {
-      actual_distance = hc_sr04_measure_cm(sensor);
-
-      if (actual_distance < 0) {
-        actual_distance = -1;
-      }
-      else {
-        actual_distance /= 100; //cm to meter
-        if (actual_distance < MIN_RANGE || actual_distance > MAX_RANGE)
-          actual_distance = MIN_RANGE;
-
-        error = desired_distance - actual_distance;
-        error_sum += error * dT;
-        error_div = (error - error_prev) / dT;
-        output = error * kp + error_sum * ki + error_div * kd;
-        error_prev = error;
-
-        output += MIN_PWM;
-        if (output > MAX_PWM)
-          output = MAX_PWM;
-
-        //send uart
-        sprintf(str, "%d", (int)output);
-        sendData(str);
-        sendData("\n");
-      }
-#ifdef DEBUG
-      static int i = 0;
-      i++;
-      if (i > DEBUG_PRINT_INTERVAL) {
-        pid_struct.error = error;
-        pid_struct.error_sum = error_sum;
-        pid_struct.error_div = error_div;
-        pid_struct.error_prev = error_prev;
-        pid_struct.desired_distance = desired_distance;
-        pid_struct.actual_distance = actual_distance;
-        pid_struct.pwm = pwm;
-        pid_struct.output = output;
-
-        esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &pid_struct, sizeof(pid_struct));
-
-        if (result == ESP_OK) {
-          printf("PID debug info send succes");
-        }
-        else {
-          printf("PID debug info send fail");
-        }
-        i = 0;
-      } 
-      vTaskDelay(dT*1000 / portTICK_PERIOD_MS);
-#endif
-    }
+    printf("actual_distance: %f\n", actual_distance);
+    vTaskDelay(portTICK_PERIOD_MS / 100);
   }
 }}
