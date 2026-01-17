b7d049695426566e3bceb1d4daa2665da49872a5
gijsdevries <gijsdvries21@gmail.com>
2026-01-17 19:16:07 +0100

    added todos and copied main.cpp from add_pid

--- main/main.cpp
+++ main/main.cpp
@@ -13,36 +13,123 @@
 #include "adc.h"
 #include "uart.h"
 
-extern "C" {void app_main(void) {
-  //TODO add mpu read tester
+#define dT 0.01
+#define DEBUG_PRINT_INTERVAL 250
+#define MIN_RANGE 0.05
+#define MAX_RANGE 2
 
-  adc_init();
-  
-  hc_sr04_config_t config_hc = {
+#define BUILTIN_LED (gpio_num_t)2
+#define DEBUG
+
+//TODO read out adc and send with espnow
+//TODO read out mpu and send with espnow
+//TODO add csv logging file for data
+//TODO make pwm rec and pid control custamisable via controller
+//TODO clean up messy ahh code
+
+float error, error_sum, error_div, error_prev, desired_distance, actual_distance, pwm, output;
+bool operation_state;
+float kp, ki, kd;
+
+extern "C" {void app_main(void) {
+  desired_distance = 0.3;
+  operation_state = false;
+
+  kp = 0;
+  ki = 0;
+  kd = 0;
+
+  pid_struct pid_struct;
+  pid_struct.msg_type = PID_DRONE;
+
+  esp_now_full_init();
+
+  hc_sr04_config_t config = {
     .trigger_pin = GPIO_NUM_4,
     .echo_pin = GPIO_NUM_5,
     .timeout_us = 30000
   };
-
-  hc_sr04_handle_t sensor = hc_sr04_init(&config_hc);
+  hc_sr04_handle_t sensor = hc_sr04_init(&config);
 
   pwm_init();
 
+  gpio_reset_pin(BUILTIN_LED);
+  gpio_set_direction(BUILTIN_LED, GPIO_MODE_OUTPUT);
+
   setPWM(0);
-  vTaskDelay(5000 / portTICK_PERIOD_MS); 
+  vTaskDelay(3000 / portTICK_PERIOD_MS);
 
-  uint8_t i = 75;
-  float actual_distance = 0;
+  bool led_state = true;
 
-  int adc;
+  while (kp == 0 && ki == 0 && kd == 0) {
+    gpio_set_level((gpio_num_t)2, led_state);
+    led_state = !led_state; 
+    vTaskDelay(200 / portTICK_PERIOD_MS);
+  }
+  printf("recieved PID values:\nkp = %f\nki = %f\nkd = %f\n", kp, ki, kd);
 
   while (1) {
-    adc = read_adc();
-    actual_distance = (float)hc_sr04_measure_cm(sensor);
-    setPWM(i);
+    gpio_set_level((gpio_num_t)2, operation_state);
 
-    printf("adc: %d   actual_distance: %f   pwm: %d\n", adc, actual_distance, i);
+    if (operation_state == false) {
+      setPWM(0);
+      vTaskDelay(10 / portTICK_PERIOD_MS);
+    }
+    else {
+      actual_distance = hc_sr04_measure_cm(sensor);
 
-    vTaskDelay(100 / portTICK_PERIOD_MS); 
+      if (actual_distance < 0) {
+        actual_distance = -1;
+      }
+      else {
+        actual_distance /= 100; //cm to meter
+        if (actual_distance < MIN_RANGE || actual_distance > MAX_RANGE)
+          actual_distance = MIN_RANGE;
+
+        error = desired_distance - actual_distance;
+        error_sum += error * dT;
+        error_div = (error - error_prev) / dT;
+        output = error * kp + error_sum * ki + error_div * kd;
+        error_prev = error;
+
+        pwm = output;
+
+        if (pwm > 255)
+          pwm = 255;
+        else if (pwm < 1)
+          pwm = 1;
+
+        setPWM(pwm);
+
+
+      }
+#ifdef DEBUG
+      static int i = 0;
+      i++;
+      if (i > DEBUG_PRINT_INTERVAL) {
+        pid_struct.error = error;
+        pid_struct.error_sum = error_sum;
+        pid_struct.error_div = error_div;
+        pid_struct.error_prev = error_prev;
+        pid_struct.desired_distance = desired_distance;
+        pid_struct.actual_distance = actual_distance;
+        pid_struct.pwm = pwm;
+        pid_struct.output = output;
+
+        printf("                distance: %f\n", actual_distance);
+
+        esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &pid_struct, sizeof(pid_struct));
+
+        if (result == ESP_OK) {
+          printf("PID debug info send succes");
+        }
+        else {
+          printf("PID debug info send fail");
+        }
+        i = 0;
+      } 
+      vTaskDelay(dT*1000 / portTICK_PERIOD_MS);
+#endif
+    }
   }
 }}
