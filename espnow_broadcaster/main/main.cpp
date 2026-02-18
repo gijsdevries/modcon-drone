a7751a157c3975d79ef8d8d9c1dbcfa1cea4c092
gijs <gijsdvries21@gmail.com>
2026-02-18 21:23:21 +0100

    messing around letting ai try

--- espnow_broadcaster/main/main.cpp
+++ espnow_broadcaster/main/main.cpp
@@ -175,7 +175,9 @@ static void rx_task(void *arg) {
       else if (data[0] == 'z') {
 	pid_factor test_pid_struct;
 	test_pid_struct.msg_type = OPERATION;
-	test_pid_struct.kp = 67;
+	test_pid_struct.kp = 0;
+	test_pid_struct.ki = 0;
+	test_pid_struct.kd = 0;
 	
         esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &test_pid_struct, sizeof(test_pid_struct));
 	printf("operation state now IDLE\n");
